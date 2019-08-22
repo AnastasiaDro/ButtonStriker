b6b4400e89f8a7b844346d9a29d1223b428ac41c
AnastasiaDro <adrogunova@gmail.com>
2019-08-22 17:29:31 +0300

    Сделано: -доделан класс MainPresenter для переменных
    Осталось:
    -  ДОДЕЛАТЬ сохранять размер кнопок и нажатие при повороте экрана!!!
    -добавить отступы слева и справа
    - сделать landscape orientation для маленьких телефонов

--- app/src/main/java/buttonstriker/devgames/ru/buttonstriker/MainPresenter.java
+++ app/src/main/java/buttonstriker/devgames/ru/buttonstriker/MainPresenter.java
@@ -9,13 +9,90 @@ public class MainPresenter {
 
 //переменные для сохранения
 //счётчик нажатий на экран
-private Integer touch = 0;
+private Integer touch;
     //счётчик нажатий на кнопку
-    private Integer press = 0;
+    private Integer press;
     //счётчик последовательных попаданий
-    private Integer strike = 0;
-    //переменная для смены направлений смещения кнопки
-    // private int mod = -1;
+    private Integer strike;
     //обнулять внутренникй стайк-счётчик
-    int oneNine = 0;
+    private int oneNine;
+
+    //размеры кнопок тоже надо сохранить ТУТ
+
+    //
+
+    private MainPresenter () {
+        setTouch(0);
+        //счётчик нажатий на кнопку
+        setPress(0);
+        //счётчик последовательных попаданий
+       setStrike(0);
+        //переменная для смены направлений смещения кнопки
+        // private int mod = -1;
+        //обнулять внутренникй стайк-счётчик
+        setOneNine(0);
+    }
+
+    //Методы для увеличения переменных на 1
+    public void incrementTouch(){
+        touch++;
+    }
+    public void incrementPress(){
+        press++;
+    }
+    public void incrementStrike(){
+        strike++;
+    }
+
+    public void incrementOneNine(){
+        oneNine++;
+    }
+
+    //геттеры  для переменных
+    public Integer getTouch(){
+        return touch;
+    }
+
+    public Integer getPress(){
+        return press;
+    }
+
+    public Integer getStrike(){
+        return strike;
+    }
+    public Integer getOneNine(){
+        return oneNine;
+    }
+
+
+    //сеттеры для переменных
+    public void setTouch(Integer touch) {
+        this.touch = touch;
+    }
+
+    public void setPress(Integer press) {
+        this.press = press;
+    }
+
+    public void setStrike(Integer strike) {
+        this.strike = strike;
+    }
+
+    public void setOneNine(int oneNine) {
+        this.oneNine = oneNine;
+    }
+
+
+    //метод сохранения???
+    public static MainPresenter getInstance(){
+        synchronized (syncobj){
+            if (instance == null){
+                instance = new MainPresenter();
+            }
+            return instance;
+        }
+    }
+
+
+
 }
