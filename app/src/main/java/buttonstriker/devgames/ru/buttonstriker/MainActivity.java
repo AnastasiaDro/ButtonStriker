243695ca9975aed16cf028d75fca70fa014c86a1
AnastasiaDro <adrogunova@gmail.com>
2019-08-23 09:16:22 +0300

    ветка перенесена в мастер НУЖНО -???Перенести логику обработки нажатий в mainPresenter, потому что в обработчике нажатия требует static... пффффф Осталось: -  ДОДЕЛАТЬ сохранять размер кнопок и нажатие при повороте экрана!!! -добавить отступы слева и справа - сделать landscape orientation для маленьких телефонов

--- app/src/main/java/buttonstriker/devgames/ru/buttonstriker/MainActivity.java
+++ app/src/main/java/buttonstriker/devgames/ru/buttonstriker/MainActivity.java
@@ -35,7 +35,7 @@ public class MainActivity extends AppCompatActivity implements View.OnTouchListe
     private TextView pressCountValue;
     private TextView strikeValue;
 
-
+//
 
     //изменитель кнопки
     MyButtonChanger myButtonChanger;
